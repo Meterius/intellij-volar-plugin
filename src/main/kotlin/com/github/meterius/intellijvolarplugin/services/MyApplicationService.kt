603092089663adb5118f9b6907a15973ccce37c1
GitHub Action <action@github.com>
2022-11-30 10:41:16 +0000

    Template cleanup

--- src/main/kotlin/com/github/meterius/intellijvolarplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/meterius/intellijvolarplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.meterius.intellijvolarplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.meterius.intellijvolarplugin.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/meterius/intellijvolarplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/meterius/intellijvolarplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.meterius.intellijvolarplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.meterius.intellijvolarplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
